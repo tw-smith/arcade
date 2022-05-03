3491b469db4272db008e9ddb6c66c38aa3aecf6f
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-05-03 22:59:00 +0100

    Refactor code for readability and styling
    Apply style guide concepts like camelCase functiona and variable names.
    Declare variables appropriately with let and const. Adjust the food
    location random number generator to stop the food appearing right at
    the edge of the game map. Add a couple of todo items.

--- snake/snake.js
+++ snake/snake.js
@@ -1,52 +1,48 @@
 let canvas, ctx;
 
 // render refresh rate per second
-let render_rate = 5;
+const render_rate = 5;
 
-// Set game world colours
+// Set game world colours and item sizes
 const BACKGROUND_COLOUR = "black";
 const SNAKE_COLOUR = "white";
+const SNAKE_HEAD_COLOUR = "green";
 const FOOD_COLOUR = "blue";
-
 const FOOD_SIZE = 10;
-const SNAKE_SIZE = 20;
-let STARTING_POSITIONS = [{x:10,y:10}, {x:9,y:10}, {x:8,y:10}];
-var nextX = 1;
-var nextY = 0;
-var tile_size = 20;
-var score = 0;
-var setInterval_ID;
+const SNAKE_SIZE = TILE_SIZE = 20;
 
-var food = {
-  x: Math.floor(Math.random() * tile_size),
-  y: Math.floor(Math.random() * tile_size),
+// Set initial values
+const STARTING_POSITIONS = [{x:10,y:10}, {x:9,y:10}, {x:8,y:10}];
+let nextX = 1;
+let nextY = 0;
+let score = 0;
+let setInterval_ID;
+let food = {
+  x: randomIntRange(2,28),
+  y: randomIntRange(2,28),
+};
+let snake = {
+  segments: STARTING_POSITIONS,
 };
 
-var snake = {
-  segments: STARTING_POSITIONS
-};
-
-
+// Initialise canvas
 function init () {
   canvas = document.querySelector('.myCanvas');
   width = canvas.width = 600;
   height = canvas.height = 600;
   ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillRect(0, 0, width, height);
 
+  // Add eventListener for keystrokes and start game
   document.addEventListener("keydown", controls)
-  setInterval_ID = game_start(render_rate);
-
-
+  setInterval_ID = gameStart(render_rate);
 }
 
 
-function game_start(render_rate) {
+function gameStart(render_rate) {
   return setInterval(render,1000/render_rate);
 }
 
-
+// Listen for keystrokes and move head one tile in required direction
 function controls(e) {
   switch (e.keyCode) {
     case 65:
@@ -83,16 +79,18 @@ function controls(e) {
   }
 }
 
-function game_over(score) {
+// Stop the game if a game over condition is reached
+function gameOver(score) {
   clearInterval(setInterval_ID);
-  ctx.font = "30px Arial";
+  ctx.font = "30px Monofett";
   ctx.fillStyle = "red";
+  ctx.textAlign = "center";
   ctx.fillText("GAME OVER",300, 300 )
 }
 
-
-
-
+function randomIntRange(min,max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 function render() {
   // reset canvas on each iteration
@@ -100,12 +98,13 @@ function render() {
   ctx.fillRect(0, 0, width, height);
 
   // write score
-  ctx.font = "30px Arial";
+  ctx.font = "30px Monofett";
+  ctx.textAlign = "center";
   ctx.fillStyle = "white";
   ctx.fillText(`Score: ${score}`,300, 30 )
 
   // move snake
- snake.segments.pop()
+  snake.segments.pop() //remove tail - // TODO: check if this prevents a collision detection with final tail segment
   newHeadPos = {
     x: (snake.segments[0].x + nextX),
     y: (snake.segments[0].y + nextY),
@@ -113,26 +112,30 @@ function render() {
   // check if snake has hit own tail
   for (var i = 0; i < snake.segments.length; i++) {
     if (newHeadPos.x == snake.segments[i].x && newHeadPos.y == snake.segments[i].y){
-      game_over(score)
+      gameOver(score)
     }
   }
   snake.segments.unshift(newHeadPos)
 
 
-  // draw snake
+  // draw snake // TODO: see if this can be incorporated into the for loop above for DRY
   for (var i = 0; i < snake.segments.length; i++) {
-    ctx.fillStyle = SNAKE_COLOUR;
-    ctx.fillRect(snake.segments[i].x*tile_size, snake.segments[i].y*tile_size, SNAKE_SIZE, SNAKE_SIZE)
+    if (i==0) {
+      ctx.fillStyle = SNAKE_HEAD_COLOUR;
+    } else {
+      ctx.fillStyle = SNAKE_COLOUR;
+    }
+    ctx.fillRect(snake.segments[i].x*TILE_SIZE, snake.segments[i].y*TILE_SIZE, SNAKE_SIZE, SNAKE_SIZE)
   }
 
   // draw Food
   ctx.fillStyle = FOOD_COLOUR;
-  ctx.fillRect(food.x*tile_size, food.y*tile_size, FOOD_SIZE, FOOD_SIZE)
+  ctx.fillRect(food.x*TILE_SIZE, food.y*TILE_SIZE, FOOD_SIZE, FOOD_SIZE)
 
-  // add to snake
+  // If hit food, add to snake and refresh food location
   if ((snake.segments[0].x == food.x) && (snake.segments[0].y == food.y)) {
-    food.x = Math.floor(Math.random() * tile_size);
-    food.y = Math.floor(Math.random() * tile_size);
+    food.x = randomIntRange(2,28);
+    food.y = randomIntRange(2,28);
     snake.segments.push(snake.segments[snake.segments.length]);
     score ++;
   }
@@ -142,7 +145,7 @@ function render() {
       (snake.segments[0].x <= 0) ||
       (snake.segments[0].y >= 30) ||
       (snake.segments[0].y <= 0)) {
-    game_over(score)
+    gameOver(score)
   }
 }
 

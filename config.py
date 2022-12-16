d6ab28753e511bfb8b718c062c2bbf4587577dba
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-12-16 17:10:37 +0000

    cleaning

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/arcade_app_dev.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '***REMOVED***'

2a6ae637ddf5196b8aad1b50594381aed6acb96b
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-12-16 16:41:24 +0000

    improves error handling and formatting

--- arcade_app/models.py
+++ arcade_app/models.py
@@ -1,30 +1,35 @@
 from arcade_app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(30))
     scores = db.relationship('Score', backref='player', lazy='dynamic')
+
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+
 class Score(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
         return '<Score {}>'.format(self.user_id)
 
+
 class MPUser(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(50), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
--- arcade_app/routes.py
+++ arcade_app/routes.py
@@ -1,62 +1,56 @@
 from flask import render_template, request, jsonify, Response, make_response
 from arcade_app import app, db
 from arcade_app.models import Score, User, MPUser
-from arcade_app.forms import HighScoreForm
 import json
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 
 from functools import wraps
+from sqlalchemy import exc
 
 import string
 import random
+from datetime import datetime, timedelta
 
 
-@app.route('/score', methods=['GET','POST'])
+@app.route('/score', methods=['GET', 'POST'])
 def score_handler():
     if request.method == 'POST':
         content = request.json
         # Check if player already exists
-        player_exists = db.session.execute(db.select(User).filter_by(user_name=content['username'])).scalars().all()
+        player_exists = db.session.execute(db.select(User).filter_by(
+            user_name=content['username'])).scalars().all()
         if player_exists:
             for row in player_exists:
                 player_id = row.id
-            s = Score(user_id = player_id, score = content['score'])
+            s = Score(user_id=player_id, score=content['score'])
             db.session.add(s)
             db.session.commit()
-            
-
 
         else:
             print("player does not exist")
             u = User(user_name=content['username'])
             db.session.add(u)
             db.session.flush()
-            s = Score(user_id=u.id, score = content['score'])
+            s = Score(user_id=u.id, score=content['score'])
             db.session.add(s)
             db.session.commit()
 
         return Response(status=201)
     elif request.method == 'GET':
-        high_scores = db.session.execute(db.select(Score.id, Score.score, User.user_name).join(User).order_by(Score.score.desc()).limit(10))
+        high_scores = db.session.execute(db.select(Score.id, Score.score, User.user_name).join(
+            User).order_by(Score.score.desc()).limit(10))
         output = [dict(row) for row in high_scores]
         output = jsonify(output)
         print(output)
         return output, 200
 
 
-
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
-
-
-# AUTH SECTION
-   # "username": "Test User"
-   # "email": "test@tw-smith.me"
-   # "password": "testpassword"
 
 
 # custom decorator for verifying token
@@ -68,48 +62,55 @@ def token_required(f):
         print(request.headers)
 
         if 'Authorization' in request.headers:
+            token = request.headers.get('Authorization')
+            token = json.loads(token)
             try:
-                token = request.headers.get('Authorization')
-                token = json.loads(token)
-                token_data = jwt.decode(token['token'], key=app.config['SECRET_KEY'], algorithms='HS256')
-                userFingerprintHash = token_data['userFingerprint']
-            except:
-                return make_response('Token is invalid!',
-                                     401,
-                                     {'WWW-Authentication': 'Bearer realm="Valid access token requried'})
-        if not token:
-            return make_response('Missing token!',
-                                  401,
-                                  {'WWW-Authentication': 'Bearer realm="Access token required"'})
+                token_data = jwt.decode(token['token'],
+                                        options={'require': ['exp', 'iss']},
+                                        key=app.config['SECRET_KEY'],
+                                        algorithms='HS256',
+                                        issuer='https://arcade.tw-smith.me')
 
-        if 'Cookie' in request.headers:
-            userFingerprint = request.cookies.get('secure-fgp')
-            if check_password_hash(userFingerprintHash, userFingerprint):
-                print("cookie hash check passed")
-
-                print("userfingerprint: " + userFingerprint)
+            except jwt.exceptions.ExpiredSignatureError:
+                return make_response('Token has expired', 401, {'WWW-Authentication': 'Bearer realm="Expired token"'})
+            except jwt.exceptions.InvalidAlgorithmError:
+                return make_response('Invalid algorithm', 401, {'WWW-Authentication': 'Bearer realm="Invalid algorithm'})
+            except jwt.exceptions.InvalidIssuedAtError:
+                return make_response('Invalid issuer', 401, {'WWW-Authentication': 'Bearer realm="Invalid issuer'})
+            except jwt.exceptions.InvalidSignatureError:
+                return make_response('Invalid signature', 401, {'WWW-Authentication': 'Bearer realm="Invalid signature"'})
+            except jwt.exceptions.MissingRequiredClaimError:
+                return make_response('Missing claim', 401, {'WWW-Authentication': 'Bearer realm="missing claim"'})
+            except jwt.exceptions.DecodeError:
+                return make_response('JWT decode error', 401, {'WWW-Authentication': 'Bearer realm="JWT decode error"'})
             else:
-                return make_response('Invalid fingerprint',
-                                     401,
-                                    {'WWW-Authentication': 'Bearer realm="Valid fingerprint required'})
+                userFingerprintHash = token_data['userFingerprint']
+
+        if not token:
+            return make_response('Missing token!', 401, {'WWW-Authentication': 'Bearer realm="Access token required"'})
+
+        if 'Cookie' not in request.headers:
+            return make_response('No fingerprint cookie', 401, {'WWW-Authentication': 'Bearer realm="fingerprint cookie"'})
+        userFingerprint = request.cookies.get('secure-fgp')
+
         if not userFingerprint:
-            return make_response('No fingerprint',
-                                  401,
-                                  {'WWW-Authentication': 'Bearer realm="Fingerprint required'})
+            return make_response('No fingerprint', 401, {'WWW-Authentication': 'Bearer realm="Fingerprint required'})
+
+        if not check_password_hash(userFingerprintHash, userFingerprint):
+            return make_response('Invalid fingerprint', 401, {'WWW-Authentication': 'Bearer realm="fingerprint error"'})
 
         try:
             print(token_data['public_id'])
-            current_user = db.session.execute(db.select(MPUser).filter_by(public_id = token_data['public_id'])).first()
+            current_user = db.session.execute(db.select(MPUser).filter_by(
+                public_id=token_data['public_id'])).first()
             print(current_user)
 
-        except:
-            return make_response('DB Error!',
-                                 500)
-        
+        except exc.SQLAlchemyError:
+            return make_response('DB Error!', 500)
+
         return f(current_user, *args, **kwargs)
     return decorated
 
-        
 
 @app.route('/users', methods=['GET'])
 @token_required
@@ -119,7 +120,7 @@ def get_all_users(current_user):
     return make_response('OK', 201)
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
@@ -131,15 +132,17 @@ def login():
 
         # check that form has data we need
         if not username or not password:
-            #return jsonify({'Message': 'No username or password'}), 401
+            # return jsonify({'Message': 'No username or password'}), 401
             return make_response('No username or password',
-                                401,
-                                {'WWW-Authentication':'Basic realm="No username/password supplied"'})
+                                 401,
+                                 {'WWW-Authentication': 'Basic realm="No username/password supplied"'})
 
         # hit DB for user
-        user = db.session.execute(db.select(MPUser).filter_by(username=username)).first()
+        user = db.session.execute(
+            db.select(MPUser).filter_by(username=username)).first()
 
-        # TODO This is messy, there must be a way to return user as a useful Python object rather than this SQLalchemy thing
+        # TODO This is messy, there must be a way to return user as a
+        # useful Python object rather than this SQLalchemy thing
         for row in user:
             pw = row.password_hash
             public_id = row.public_id
@@ -147,46 +150,43 @@ def login():
         # return 401 if user does not exist
         if not user:
             return make_response('No such user exists',
-                                  401,
-                                  {'WWW-Authenticate' : 'Basic realm="User does not exist"'})
+                                 401,
+                                 {'WWW-Authenticate': 'Basic realm="User does not exist"'})
 
         # check password hash and create JWT token
         if check_password_hash(pw, password):
 
             # TODO is the fingerprint ok as random string or does it need to be random hex?
             # Generate user fingerprint
-            userFingerprint = ''.join(random.choices(string.ascii_letters, k = 50))
-            fingerPrintCookie = 'secure-fgp=' + userFingerprint + "; SameSite=Strict; HttpOnly; Secure"
+            userFingerprint = ''.join(
+                random.choices(string.ascii_letters, k=50))
+            fingerPrintCookie = 'secure-fgp=' + userFingerprint + \
+                "; SameSite=Strict; HttpOnly; Secure"
 
             # Create hash of fingerprint
-            #TODO is this hashing algo sufficient
-            userFingerprintHash = generate_password_hash(userFingerprint,'SHA256')
+            # TODO is this hashing algo sufficient
+            userFingerprintHash = generate_password_hash(
+                userFingerprint, 'SHA256')
 
             # Encode token
             # TODO implement the rest of the items required in the JWT spec
             # TODO should we be using a different crypto algo?
             jwt_token = jwt.encode({
+                'iss': 'https://arcade.tw-smith.me',
+                'iat': datetime.utcnow(),
+                'exp': datetime.utcnow() + timedelta(minutes=15),
                 'public_id': public_id,
                 'userFingerprint': userFingerprintHash,
+            }, app.config['SECRET_KEY'],
+                headers={
+                'typ': 'JWT',
                 'alg': 'HS256'
-                # TODO put token expiry time in
-            }, app.config['SECRET_KEY'])
-
+            }
+            )
 
             return make_response(jsonify({'token': jwt_token}),
                                  201,
                                  {'Set-Cookie': fingerPrintCookie})
-
-
-
-
-
-        
-
-
-
-    
-
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -202,27 +202,23 @@ def signup():
         password = data['password']
 
         # Check if username or email already exists in DB
-        username_check = db.session.execute(db.select(MPUser).filter_by(username = username)).first()
-        email_check = db.session.execute(db.select(MPUser).filter_by(email = email)).first()
-        
+        username_check = db.session.execute(
+            db.select(MPUser).filter_by(username=username)).first()
+        email_check = db.session.execute(
+            db.select(MPUser).filter_by(email=email)).first()
+
         # If username or email already exists then throw error, otherwise create user
         if username_check or email_check:
             print('username or email already exists')
             return make_response('User already exists!', 409)
         else:
             user = MPUser(
-                username = username,
-                email = email,
-                public_id = str(uuid.uuid4()),
-                password_hash = generate_password_hash(password)
+                username=username,
+                email=email,
+                public_id=str(uuid.uuid4()),
+                password_hash=generate_password_hash(password)
             )
             db.session.add(user)
             db.session.commit()
             print('sign up ok')
             return make_response('User registration sucessfull', 201)
-            
-
-
-
-
-

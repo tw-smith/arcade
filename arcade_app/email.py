87441ebb0b25262023fc48f149d60ea33006bc51
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-12-27 13:39:20 +0000

    async email support

--- arcade_app/email.py
+++ arcade_app/email.py
@@ -1,9 +1,18 @@
 from flask_mail import Message
 from arcade_app import mail
+from threading import Thread
+from flask import current_app
+
+def send_async_email(app, msg):
+    with app.app_context():
+        #mail.send(msg)
+        print(msg.body)
+
+
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject=subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    print(msg.body)
-    #mail.send(msg)
+    Thread(target=send_async_email, 
+           args=(current_app._get_current_object(), msg)).start()

132cba1c300633f6e2dd2c1cd89aeedb53ef4fc1
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-01-11 15:18:42 +0000

    adds lobby host and dual host check

--- arcade_app/game/routes.py
+++ arcade_app/game/routes.py
@@ -16,16 +16,30 @@ def singleplayer():
 
 @bp.route('/multiplayer/lobby/', methods=['GET'])
 @login_required
-def multiplayer_lobby(is_host=False):
+def multiplayer_lobby():
     session['lobby_id'] = request.args.get('lobby_id')
-    lobby = db.session.execute(db.select(Lobby).filter_by(public_id=session.get('lobby_id'))).first()
+    if request.args.get('is_host') == 'True':
+        is_host = True
+    else:
+        is_host = False
+    #lobby = db.session.execute(db.select(Lobby).filter_by(public_id=session.get('lobby_id'))).first()
+    lobby = db.session.execute(db.select(Lobby).filter_by(public_id=session.get('lobby_id'))).scalars().first()
+    print(type(lobby.active_user.first()))
     if not lobby:
         flash('No such lobby exists!')
         return redirect(url_for('main.matchmake'))
-    number_players = db.session.query(ActiveUsers).filter_by(lobby_id=session.get('lobby_id')).count()
+    #number_players = db.session.query(ActiveUsers).filter_by(lobby_id=session.get('lobby_id')).count()
+    players = db.session.execute(db.select(ActiveUsers).filter_by(lobby_id=session.get('lobby_id'))).scalars().all()
+    number_players = len(players)
+    print(number_players)
     if number_players >= 2:
         flash('Lobby full!')
         return redirect(url_for('main.matchmake'))
+    if is_host:
+        for player in players:
+            if player.is_host:
+                flash('This lobby already has a host!')
+                return redirect(url_for('main.matchmake'))
     new_active_user = ActiveUsers(player_id=current_user.username, lobby_id=session.get('lobby_id'), is_host=is_host)
     db.session.add(new_active_user)
     db.session.commit()
